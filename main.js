2abfb2b0a1e053ec3d1f0fbcd1ddf06d1b45ddcc
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2019-02-04 00:35:40 -0500

    Add an array.map version when batchSize == 0

--- main.js
+++ main.js
@@ -46,16 +46,23 @@ document
 
         g_startTime = performance.now()
 
-        for (var i = 0; i < g_vects.length; i += g_batchSize) {
-            const start = i
-            const end = i + g_batchSize
-            g_worker.postMessage({
-                type: 'transform',
-                start: start,
-                end: end,
-                vects: g_vects.slice(start, end)
-            })
+        if (g_batchSize == 0) {
+            g_vects.map(transform)
+            markReplaced(g_vecCount)
+        } else {
+            for (var i = 0; i < g_vects.length; i += g_batchSize) {
+                const start = i
+                const end = i + g_batchSize
+                g_worker.postMessage({
+                    type: 'transform',
+                    start: start,
+                    end: end,
+                    vects: g_vects.slice(start, end)
+                })
+            }
+
         }
+
 
     })
 
@@ -74,4 +81,22 @@ function markReplaced(count) {
 
 function randBetween(min, max) {
     return Math.random() * (+max - +min) + +min
+}
+
+// Duplicated from worker.js, and not split out into a module.
+// Because I am _lazy_.
+const TRANSFORM = [
+    [0.1527236, -0.9116594, 0.3815136, 4.0000000],
+    [0.9756048, 0.2006826, 0.0890043, -7.5000000],
+    [-0.1577047, 0.3586135, 0.9200683, 3.2000000],
+    [0.0000000, 0.0000000, 0.0000000, 1.0000000],
+]
+
+function transform(v) {
+    let ret = [0, 0, 0, 0]
+    ret[0] = (v[0] * TRANSFORM[0][0]) + (v[1] * TRANSFORM[0][1]) + (v[2] * TRANSFORM[0][2]) + (v[3] * TRANSFORM[0][3])
+    ret[1] = (v[0] * TRANSFORM[1][0]) + (v[1] * TRANSFORM[1][1]) + (v[2] * TRANSFORM[1][2]) + (v[3] * TRANSFORM[1][3])
+    ret[2] = (v[0] * TRANSFORM[2][0]) + (v[1] * TRANSFORM[2][1]) + (v[2] * TRANSFORM[2][2]) + (v[3] * TRANSFORM[2][3])
+    ret[3] = (v[0] * TRANSFORM[3][0]) + (v[1] * TRANSFORM[3][1]) + (v[2] * TRANSFORM[3][2]) + (v[3] * TRANSFORM[3][3])
+    return ret
 }
